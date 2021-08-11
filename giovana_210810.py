ca694443ef46f6a7f8a73938d67d0d1159550cb7
giovana10 <88749160+giovana10@users.noreply.github.com>
2021-08-11 14:10:53 -0300

    Add files via upload

---
+++ giovana_210810.py
@@ -0,0 +1,19 @@
+#question 3
+code= list(range(2,13,2))
+#question 4
+for value in range(1,11):
+ if value in code :
+     print(value)
+#question 5
+for value in range(1,7):
+    print(value)
+#question 6
+for value in range(1,7):
+    if value <3:
+        print(str(value)+ " is smaller than 5 and is not bigger than 2")
+    elif value <5:
+        print(str(value)+ " is smaller than 5 and is bigger than 2")
+    else:
+         print(str(value)+ " is not smaller than 5 and is bigger than 2")
+        
+
